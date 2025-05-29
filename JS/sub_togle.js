588a904bbe406beab89a13c22defc5f0a2bbc403
BHLim08 <illust_gyubin@naver.com>
2025-05-29 10:15:51 +0900

    25-05-29

--- JS/sub_togle.js
+++ JS/sub_togle.js
@@ -1,18 +1,34 @@
 
 //디테일 버튼 토글
 
+// 책 소개
 $('.more_btn').click(function(){
 
     let book = $('.book_intro');
     let bookhight = book.height();
 
-    if( bookhight === 300 ){ 
-        book.stop().animate({height:'780px'},3000)
+    if( bookhight < 200 ){ 
+        book.stop().animate({height:'790px'},3000)
     }
-   
-
-    // $('.book_intro').stop().animate({height:'300px'},3000)
-    
+    if (bookhight === 669){
+         book.stop().animate({height:'280px'},3000)
+    }
 }) 
+
+// 서평
+
+$('.more_col_btn').click(function(){
+
+    let book = $('.book_columns');
+    let bookhight = book.height();
+
+    if( bookhight < 200 ){ 
+        book.stop().animate({height:'3800px'},3000)
+    }
+    if (bookhight === 3679){
+         book.stop().animate({height:'280px'},3000)
+    }
+}) 
+
 
 
