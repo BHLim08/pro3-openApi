1c0a0d6502ade665c6c8527c96b093e7e24bfbf7
BHLim08 <illust_gyubin@naver.com>
2025-05-30 11:14:46 +0900

    25-05-30

--- JS/sub_nav.js
+++ JS/sub_nav.js
@@ -15,27 +15,7 @@ $(document).ready(function () {
   
     $('.sub_nav a').eq(0).addClass('active_sc');
 
-    $('.sub_nav a').click(function () {
-        
+    $('.sub_nav li').click(function () {       
         $(this).addClass('active_suv_nav').siblings().removeClass('active_suv_nav');
     });
 
-
-    // 스크롤 헤더 체인지
-
-    let currentScrollTop = 0;
-    $(window).scroll(function () {
-        currentScrollTop = $(window).scrollTop();
-        if (currentScrollTop < 1000) {
-            $(".sub_nav a").eq(0).addClass("active_suv_nav");
-        } else {
-             $(this).addClass('active_suv_nav').siblings().removeClass('active_suv_nav');
-        }
-
-        if (currentScrollTop > 2000 && currentScrollTop < 3094) {
-            $("header").fadeOut();
-        } else {
-            $("header").fadeIn();
-        }
-
-    });
